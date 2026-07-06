4a03d2e83d9dd0c0389785f180c64d06f6dd91a5
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "palo_alto_virtual_network_appliances" {
   description = "All palo_alto_virtual_network_appliance resources"
   value       = azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances
 }
+output "palo_alto_virtual_network_appliances_name" {
+  description = "List of name values across all palo_alto_virtual_network_appliances"
+  value       = [for k, v in azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances : v.name]
+}
+output "palo_alto_virtual_network_appliances_virtual_hub_id" {
+  description = "List of virtual_hub_id values across all palo_alto_virtual_network_appliances"
+  value       = [for k, v in azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances : v.virtual_hub_id]
+}
 
