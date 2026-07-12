2cf48e79471fb287d0c02dd17ed213abc7098115
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "palo_alto_virtual_network_appliances" {
-  description = "All palo_alto_virtual_network_appliance resources"
-  value       = azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances
-}
 output "palo_alto_virtual_network_appliances_name" {
-  description = "List of name values across all palo_alto_virtual_network_appliances"
-  value       = [for k, v in azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances : v.name]
+  description = "Map of name values across all palo_alto_virtual_network_appliances, keyed the same as var.palo_alto_virtual_network_appliances"
+  value       = { for k, v in azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances : k => v.name }
 }
 output "palo_alto_virtual_network_appliances_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all palo_alto_virtual_network_appliances"
-  value       = [for k, v in azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all palo_alto_virtual_network_appliances, keyed the same as var.palo_alto_virtual_network_appliances"
+  value       = { for k, v in azurerm_palo_alto_virtual_network_appliance.palo_alto_virtual_network_appliances : k => v.virtual_hub_id }
 }
 
